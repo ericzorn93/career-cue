40db2173fbcb442b4504571cd1435b1ab1602aeb
Eric Zorn <zornwebdev@gmail.com>
2024-11-27 16:24:29 -0500

    feat: adding ecmascript code gen

---
+++ packages/proto-gen/es/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'proto-gen-es',
+  preset: '../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/packages/proto-gen/es',
+};
---
+++ packages/proto-gen/es/src/index.ts
@@ -0,0 +1,5 @@
+// TODO: Fix
+
+// export * from './accounts';
+// export * from './billing';
+// export * from './common';
