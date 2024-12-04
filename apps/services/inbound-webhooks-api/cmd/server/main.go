80e5999d7bca506d222c395deeaff7cb7f22f175
Eric Zorn <zornwebdev@gmail.com>
2024-12-04 13:18:18 -0500

    feat: optimizing the docker compose and different volumes

--- apps/services/inbound-webhooks-api/cmd/server/main.go
+++ apps/services/inbound-webhooks-api/cmd/server/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v3"
+)
 
 func Hello(name string) string {
 	result := "Hello " + name
@@ -9,4 +14,11 @@ func Hello(name string) string {
 
 func main() {
 	fmt.Println(Hello("inbound-webhooks-api"))
+
+	app := fiber.New()
+	app.Get("/", func(c fiber.Ctx) error {
+		return c.SendString("Hello world from Inbound Webhooks API!")
+	})
+
+	log.Fatal(app.Listen(":3000"))
 }
