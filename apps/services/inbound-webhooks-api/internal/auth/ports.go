bf171fb30837e9e3ed81a703fa9701ad528a8851
Eric Zorn <zornwebdev@gmail.com>
2024-12-16 13:23:09 -0500

    fix: adding port docstring

--- apps/services/inbound-webhooks-api/internal/auth/ports.go
+++ apps/services/inbound-webhooks-api/internal/auth/ports.go
@@ -7,6 +7,15 @@ import (
 	pb "libs/backend/proto-gen/go/webhooks/inboundwebhooksapi/v1"
 )
 
+// AuthHandler is the interface for the service methods on the
+// gRPC handler
+type AuthHandler interface {
+	UserRegistered(
+		ctx context.Context,
+		req *pb.UserRegisteredRequest,
+	) (*commonv1.Empty, error)
+}
+
 // AuthServicePort is the interface that defines
 // how the authentication provider interacts with
 // the internal system
@@ -14,11 +23,4 @@ type AuthServicePort interface {
 	// SendUserRegistered will convert the domain type type and send to
 	// the message broker
 	SendUserRegistered(ctx context.Context, user entities.User) error
-}
-
-type AuthHandler interface {
-	UserRegistered(
-		ctx context.Context,
-		req *pb.UserRegisteredRequest,
-	) (*commonv1.Empty, error)
 }
