ca94cd289e7f79e2658de17cf59c1da2a971a8ec
Eric Zorn <zornwebdev@gmail.com>
2024-12-17 16:31:43 -0500

    feat: adding event package

---
+++ libs/backend/eventing/auth.go
@@ -0,0 +1,12 @@
+package eventing
+
+// Event Producer/Consumer
+const (
+	AuthExchange      EventInfrastructureName = "authExchange"
+	RegistrationQueue EventInfrastructureName = "registrationQueue"
+)
+
+// Event Names
+const (
+	EventNameUserRegistered EventName = "career-cue.auth.userRegistered"
+)
