54549a31f0795385a4cbc907141ea0c077a685da
Eric Zorn <zornwebdev@gmail.com>
2024-12-20 16:48:40 -0500

    feat: adding all user domain types for user registered event

--- apps/services/inbound-webhooks-api/internal/domain/user.go
+++ apps/services/inbound-webhooks-api/internal/domain/user.go
@@ -32,6 +32,62 @@ func WithUserLastName(lastName string) UserOption {
 	}
 }
 
+// WithUserNickname adds the user's nickname to the struct
+func WithUserNickname(nickname string) UserOption {
+	return func(u *User) {
+		u.Nickname = nickname
+	}
+}
+
+// WithUserUsername adds the user's username to the struct
+func WithUserUsername(username string) UserOption {
+	return func(u *User) {
+		u.Username = username
+	}
+}
+
+// WithEmailAddress adds the user's email address to the struct
+func WithEmailAddress(EmailAddress string) UserOption {
+	return func(u *User) {
+		u.EmailAddress = EmailAddress
+	}
+}
+
+// WithEmailAddressVerified adds the user's email address verification status to the struct
+func WithEmailAddressVerified(EmailAddressVerified bool) UserOption {
+	return func(u *User) {
+		u.EmailAddressVerified = EmailAddressVerified
+	}
+}
+
+// WithPhoneNumber adds the user's phone number to the struct
+func WithPhoneNumber(PhoneNumber string) UserOption {
+	return func(u *User) {
+		u.PhoneNumber = PhoneNumber
+	}
+}
+
+// WithPhoneNumberVerified adds the user's phone number verification status to the struct
+func WithPhoneNumberVerified(PhoneNumberVerified bool) UserOption {
+	return func(u *User) {
+		u.PhoneNumberVerified = PhoneNumberVerified
+	}
+}
+
+// WithStrategy adds the user's authentication strategy to the struct
+func WithStrategy(Strategy string) UserOption {
+	return func(u *User) {
+		u.Strategy = Strategy
+	}
+}
+
+// WithMetadata adds the user's metadata to the struct
+func WithMetadata(Metadata map[string]any) UserOption {
+	return func(u *User) {
+		u.Metadata = Metadata
+	}
+}
+
 // NewUser is the constructor for a User struct
 func NewUser(opts ...UserOption) User {
 	u := User{}
