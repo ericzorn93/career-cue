bef308a13920ef7e6870250199d242abba9f6fd6
Eric Zorn <zornwebdev@gmail.com>
2024-12-23 14:01:36 -0500

    fix: generator

--- tools/backend-service/src/generators/service-gen/files/internal/adapters/connectrpc/generic_handler.go
+++ tools/backend-service/src/generators/service-gen/files/internal/adapters/connectrpc/generic_handler.go
@@ -1,16 +1,16 @@
 package connectrpc
 
 import (
-	"libs/boot/pkg/logger"
+	"libs/boot"
 )
 
 // AuthHandler handles all gRPC endpoints for inbound webhooks
 type GenericHandler struct {
-	Logger logger.Logger
+	Logger boot.Logger
 }
 
 // NewAuthHandler will return a pointer to the inbound webhooks API server
-func NewAuthHandler(logger logger.Logger) *GenericHandler {
+func NewAuthHandler(logger boot.Logger) *GenericHandler {
 	return &GenericHandler{
 		Logger: logger,
 	}
