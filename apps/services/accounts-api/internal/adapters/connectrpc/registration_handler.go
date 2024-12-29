b616ebc02160e524f1bfa9a9c48aadb66cb389b7
Eric Zorn <zornwebdev@gmail.com>
2024-12-29 16:58:30 -0500

    feat: handle user

--- apps/services/accounts-api/internal/adapters/connectrpc/registration_handler.go
+++ apps/services/accounts-api/internal/adapters/connectrpc/registration_handler.go
@@ -1,24 +1,28 @@
 package connectrpc
 
 import (
+	"apps/services/accounts-api/internal/models"
 	"context"
 	"libs/backend/boot"
 	accountsapiv1 "libs/backend/proto-gen/go/accounts/accountsapi/v1"
 	"libs/backend/proto-gen/go/accounts/accountsapi/v1/accountsapiv1connect"
 
 	"connectrpc.com/connect"
+	"gorm.io/gorm"
 )
 
 // AuthHandler handles all gRPC endpoints for inbound webhooks
 type RegistrationServiceHandler struct {
 	accountsapiv1connect.UnimplementedRegistrationServiceHandler
 	Logger boot.Logger
+	DB     *gorm.DB
 }
 
 // NewRegistrationHandler will return a pointer to the inbound webhooks API server
-func NewRegistrationHandler(logger boot.Logger) *RegistrationServiceHandler {
+func NewRegistrationHandler(logger boot.Logger, db *gorm.DB) *RegistrationServiceHandler {
 	return &RegistrationServiceHandler{
 		Logger: logger,
+		DB:     db,
 	}
 }
 
@@ -28,5 +32,6 @@ func (r *RegistrationServiceHandler) CreateAccount(
 	req *connect.Request[accountsapiv1.CreateAccountRequest],
 ) (*connect.Response[accountsapiv1.CreateAcountResponse], error) {
 	r.Logger.Info("CreateAccount called")
+	r.DB.Save(&models.Account{FirstName: req.Msg.FirstName})
 	return connect.NewResponse(&accountsapiv1.CreateAcountResponse{IsSuccess: true}), nil
 }
