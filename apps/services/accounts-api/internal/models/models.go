41af8ae909c3c34a4fba86cf271c971e16441c59
Eric Zorn <zornwebdev@gmail.com>
2024-12-30 15:25:20 -0500

    fix: email address verified boolean

--- apps/services/accounts-api/internal/models/models.go
+++ apps/services/accounts-api/internal/models/models.go
@@ -8,8 +8,9 @@ import (
 // Account is our model, which corresponds to the "accounts" table
 type Account struct {
 	gorm.Model
-	ID       uuid.UUID `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
-	CommonID uuid.UUID `gorm:"type:uuid;"`
-	UserName string    `gorm:"unique;index:idx_user_name;index:idx_user_name_email"`
-	Email    string    `gorm:"unique;index:idx_email;index:idx_user_name_email"`
+	ID                   uuid.UUID `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
+	CommonID             uuid.UUID `gorm:"type:uuid;"`
+	UserName             string    `gorm:"unique;index:idx_user_name;index:idx_user_name_email"`
+	EmailAddress         string    `gorm:"unique;index:idx_email;index:idx_user_name_email"`
+	EmailAddressVerified bool      `gorm:"default:false"`
 }
