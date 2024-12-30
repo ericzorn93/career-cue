c332441a376653718c26ea7374f19dd98fc442cf
Eric Zorn <zornwebdev@gmail.com>
2024-12-30 15:48:58 -0500

    feat: adding common_id index

--- apps/services/accounts-api/internal/models/models.go
+++ apps/services/accounts-api/internal/models/models.go
@@ -9,7 +9,7 @@ import (
 type Account struct {
 	gorm.Model
 	ID           uuid.UUID `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
-	CommonID     uuid.UUID `gorm:"type:uuid;"`
+	CommonID     uuid.UUID `gorm:"type:uuid;index:idx_common_id"`
 	UserName     string    `gorm:"unique;index:idx_user_name;index:idx_user_name_email"`
 	EmailAddress string    `gorm:"unique;index:idx_email;index:idx_user_name_email"`
 }
