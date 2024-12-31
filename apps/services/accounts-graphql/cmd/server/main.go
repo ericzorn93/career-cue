f20efd22cad53525a1e0ec062f6c86719ab2a0e5
Eric Zorn <zornwebdev@gmail.com>
2024-12-31 14:03:33 -0500

    feat: adding graphql service for accounts

---
+++ apps/services/accounts-graphql/cmd/server/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"apps/services/accounts-graphql/internal/config"
+	"context"
+	"errors"
+	"log"
+	"log/slog"
+	"os"
+
+	"connectrpc.com/connect"
+	"connectrpc.com/validate"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"libs/backend/boot"
+)
+
+// serviceName is the name of the microservice
+const serviceName = "accounts-graphql"
+
+func run() error {
+	// Application Context
+	ctx := context.Background()
+
+	// Create logger
+	logger := boot.NewSlogger()
+
+	// Construct config
+	config, err := config.NewConfig()
+	if err != nil {
+		logger.Error("Trouble constructing config")
+		os.Exit(1)
+	}
+
+	// Connect Interceptors
+	validationInterceptor, err := validate.NewInterceptor()
+	if err != nil {
+		logger.Error("Cannot set up validation interceptor", slog.Any("error", err))
+		return err
+	}
+	_ = []connect.HandlerOption{
+		connect.WithInterceptors(validationInterceptor),
+	}
+
+	// Initialize the gRPC Options
+	bootService := boot.
+		NewBuildServiceBuilder().
+		SetServiceName(serviceName).
+		SetLogger(logger).
+		SetAMQPOptions(boot.AMQPOptions{
+			ConnectionURI: config.AMQPUrl,
+			OnConnectionCallback: func(params boot.AMQPCallBackParams) error {
+				params.Logger.Info("AMQP connected successfully")
+
+				// Set up all AMQP queues and exchanges
+
+				params.Logger.Info("Set up all AMQP queues and exchanges")
+
+				return nil
+			},
+		}).
+		SetConnectRPCOptions(boot.ConnectRPCOptions{
+			Port: 3000,
+			TransportCredentials: []credentials.TransportCredentials{
+				insecure.NewCredentials(),
+			},
+			Handlers: []boot.ConnectRPCHandler{
+				func(params boot.ConnectRPCHandlerParams) error {
+					if !params.AMQPController.IsConnected() {
+						errMsg := "AMQP not conntected"
+						logger.Error(errMsg)
+						return errors.New(errMsg)
+					}
+
+					// Set up all ConnectRPC Handlers
+
+					return nil
+				},
+			},
+		}).
+		SetBootCallbacks([]boot.BootCallback{
+			func(params boot.BootCallbackParams) error {
+				params.Logger.Info("Service booted successfully", slog.String("serviceName", serviceName))
+				return nil
+			},
+		}).
+		Build()
+
+	return bootService.Start(ctx)
+}
+
+func main() {
+	if err := run(); err != nil {
+		log.Printf("Cannot start service %s", serviceName)
+		os.Exit(1)
+	}
+}
