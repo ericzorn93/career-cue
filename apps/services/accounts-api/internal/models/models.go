b523a61320fe5213360f646f43238d110d81106a
Eric Zorn <zornwebdev@gmail.com>
2024-12-31 10:40:19 -0500

    fix: make fields non-nullable in accounts DB

--- apps/services/accounts-api/internal/models/models.go
+++ apps/services/accounts-api/internal/models/models.go
@@ -9,7 +9,7 @@ import (
 type Account struct {
 	gorm.Model
 	ID           uuid.UUID `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
-	CommonID     uuid.UUID `gorm:"type:uuid;index:idx_common_id"`
-	UserName     string    `gorm:"unique;index:idx_user_name;index:idx_user_name_email"`
-	EmailAddress string    `gorm:"unique;index:idx_email;index:idx_user_name_email"`
+	CommonID     uuid.UUID `gorm:"type:uuid;index:idx_common_id;not null;"`
+	UserName     string    `gorm:"unique;index:idx_user_name;index:idx_user_name_email;not null;"`
+	EmailAddress string    `gorm:"unique;index:idx_email;index:idx_user_name_email;not null;"`
 }
