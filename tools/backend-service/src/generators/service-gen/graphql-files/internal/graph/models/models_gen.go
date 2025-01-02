3b10e5efe5e587f96b6419102078353cf2127eda
Eric Zorn <zornwebdev@gmail.com>
2025-01-02 16:17:26 -0500

    feat: cleanup graphql files

--- tools/backend-service/src/generators/service-gen/graphql-files/internal/graph/models/models_gen.go
+++ tools/backend-service/src/generators/service-gen/graphql-files/internal/graph/models/models_gen.go
@@ -2,19 +2,9 @@
 
 package models
 
-type Account struct {
-	ID    string `json:"id"`
-	Email string `json:"email"`
-}
-
 type Mutation struct {
 }
 
 type Query struct {
 }
 
-type Todo struct {
-	ID   string `json:"id"`
-	Done bool   `json:"done"`
-	Text string `json:"text"`
-}
